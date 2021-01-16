bdcfb4b09d37936436f17dacf5d050b7e38492cd
KangDroid <hyunwoo286@gmail.com>
2021-01-16 20:19:28 +0900

    Initial Server

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    val springBootVersion = "2.1.7.RELEASE"
+
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:${springBootVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-allopen:1.4.21")
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "me.kangdroid"
+version = "1.0-SNAPSHOT"
+
+apply {
+    plugin("kotlin-spring")
+    plugin("org.springframework.boot")
+    plugin("io.spring.dependency-management")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile("org.springframework.boot:spring-boot-starter-web")
+    compile("org.springframework.boot:spring-boot-starter-data-jpa")
+    compile("com.h2database:h2")
+    compile("org.springframework.session:spring-session-jdbc")
+    testCompile("org.springframework.security:spring-security-test")
+    testCompile("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
