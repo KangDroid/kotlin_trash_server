93b36ef73d2b2a98a8392339d235d7ff772e9138
KangDroid <hyunwoo286@gmail.com>
2021-02-02 14:11:28 +0900

    Use Groovy

--- src/main/kotlin/com/kangdroid/server/domain/TrashData.kt
+++ src/main/kotlin/com/kangdroid/server/domain/TrashData.kt
@@ -1,6 +1,10 @@
 package com.kangdroid.server.domain
 
-import javax.persistence.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Column
 
 @Entity
 class TrashData(
